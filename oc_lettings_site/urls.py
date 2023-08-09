2bb420780149784f645c76f3b8f2ec921acd027b
toutouff <63313022+toutouff@users.noreply.github.com>
2023-08-09 15:22:40 +0200

    move of profiles views and urls into dedicated app

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('letting/',include('lettings.urls')),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('profiles/',include('profiles.urls')),
     path('admin/', admin.site.urls),
 ]
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,9 +1,4 @@
 from django.shortcuts import render
-from profiles.models import Profile
-from lettings.models import Address
-from lettings.models import Letting
-
-
 
 
 # Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis leo consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros,
@@ -14,15 +9,4 @@ def index(request):
 
 # Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex, sed consequat libero pulvinar eget. Fusc
 # faucibus, urna quis auctor pharetra, massa dolor cursus neque, quis dictum lacus d
-def profiles_index(request):
-    profiles_list = Profile.objects.all()
-    context = {'profiles_list': profiles_list}
-    return render(request, 'profiles_index.html', context)
 
-# Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac
-# laoreet neque quis, pellentesque dui. Nullam facilisis pharetra vulputate. Sed tincidunt, dolor id facilisis fringilla, eros leo tristique lacus,
-# it. Nam aliquam dignissim congue. Pellentesque habitant morbi tristique senectus et netus et males
-def profile(request, username):
-    profile = Profile.objects.get(user__username=username)
-    context = {'profile': profile}
-    return render(request, 'profile.html', context)
